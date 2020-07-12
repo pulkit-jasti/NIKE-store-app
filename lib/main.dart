5c73081d4485c4ad6d281d01d543485b4d50e3fe
pulkit-jasti <31801730+pulkit-jasti@users.noreply.github.com>
2020-07-12 15:28:32 +0530

    icon update

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,14 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import 'package:flutter_launcher_icons/android.dart';
+import 'package:flutter_launcher_icons/constants.dart';
+import 'package:flutter_launcher_icons/custom_exceptions.dart';
+import 'package:flutter_launcher_icons/ios.dart';
+import 'package:flutter_launcher_icons/main.dart';
+import 'package:flutter_launcher_icons/utils.dart';
+import 'package:flutter_launcher_icons/xml_templates.dart';
+
 import './product_card.dart';
 import './accordion.dart';
 
